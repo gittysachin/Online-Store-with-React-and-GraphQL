a63e303c983d580ca8fbe2d89d4658ca46849bc9
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-06-29 18:11:45 +0530

    Locking Down DeleteItem Permissions

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -3,8 +3,8 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { ALL_ITEMS_QUERY } from './Items';
 
-const DELETE_ITEM_MUTAITON = gql`
-  mutation DELETE_ITEM_MUTAITON($id: ID!) {
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
     deleteItem(id: $id) {
       id
     }
@@ -16,26 +16,27 @@ class DeleteItem extends Component {
     // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
     const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
-    // 2. filter the deleted item out of the page
+    console.log(data, payload);
+    // 2. Filter the deleted itemout of the page
     data.items = data.items.filter(
       item => item.id !== payload.data.deleteItem.id
     );
-    // 3. put the items back
+    // 3. Put the items back!
     cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
   };
   render() {
     return (
       <Mutation
-        mutation={DELETE_ITEM_MUTAITON}
-        variables={{
-          id: this.props.id
-        }}
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
         update={this.update}>
         {(deleteItem, { error }) => (
           <button
             onClick={() => {
               if (confirm('Are you sure you want to delete this item?')) {
-                deleteItem();
+                deleteItem().catch(err => {
+                  alert(err.message);
+                });
               }
             }}>
             {this.props.children}
